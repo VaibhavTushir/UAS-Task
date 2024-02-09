35438d8e974285ca5897fb16fce2bcdd682eee27
VaibhavTushir <142437028+VaibhavTushir@users.noreply.github.com>
2024-02-09 22:47:30 +0530

    Update UAS.py

--- UAS.py
+++ UAS.py
@@ -1,61 +1,17 @@
-# First we import the  OpenCV module
-import cv2
-#Now we import the numpy module and call it np
-import numpy as np
-
-#Then the image is loaded
-img = cv2.imread('sample_image.jpeg')
-
-#After loading the image is converted to HSV
-hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-
-#We need to define the lower bounds and upper bounds for green and brown region
-lower_brown = np.array([10, 50, 50])
-upper_brown = np.array([20, 255, 255])
-lower_green = np.array([40, 50, 50])
-upper_green = np.array([70, 255, 255])
-
-#A mash is to be created for both the regions
-mask_brown = cv2.inRange(hsv, lower_brown, upper_brown)
-mask_green = cv2.inRange(hsv, lower_green, upper_green)
-
-#The contours have to be found in the masks
-contours_brown, _ = cv2.findContours(mask_brown, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-contours_green, _ = cv2.findContours(mask_green, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-#Contours are drawn on the original image
-img_contours = img.copy()
-cv2.drawContours(img_contours, contours_brown, -1, (0, 0, 255), 2)
-cv2.drawContours(img_contours, contours_green, -1, (0, 255, 0), 2)
-
-# Loop through the contours and detect the color of each house
-houses_brown = []
-houses_green = []
-for contour in contours_brown:
-    x, y, w, h = cv2.boundingRect(contour)
-    house_color = img[y + h // 2, x + w // 2]
-    if np.array_equal(house_color, [255, 0, 0]):  # Blue house
-        houses_brown.append(2)
-    elif np.array_equal(house_color, [0, 0, 255]):  # Red house
-        houses_brown.append(1)
-for contour in contours_green:
-    x, y, w, h = cv2.boundingRect(contour)
-    house_color = img[y + h // 2, x + w // 2]
-    if np.array_equal(house_color, [255, 0, 0]):  # Blue house
-        houses_green.append(2)
-    elif np.array_equal(house_color, [0, 0, 255]):  # Red house
-        houses_green.append(1)
-
-#Calculation of total priority of houses on the brown and green regions
-priority_brown = sum(houses_brown)
-priority_green = sum(houses_green)
-
-n_houses=np.array([houses_brown,houses_green])
-
-#Finally display of output image
-cv2.imshow('Original Image', img)
-cv2.imshow('Brown Region', mask_brown)
-cv2.imshow('Green Region', mask_green)
-cv2.imshow('Contours', img_contours)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+import cv2 as cv
+import numpy as np 
+img=cv.imread(r"D:\Coding\UAS\7.jpeg")
+img=cv.resize(img,None,fx=0.9,fy=0.9)
+gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+gray=255-gray
+ret,binary=cv.threshold(gray,0,255,cv.THRESH_BINARY+cv.THRESH_OTSU)
+contours, hierarchy=cv.findContours(binary, mode=cv.RETR_TREE, method =cv.CHAIN_APPROX_NONE)
+print("Length of contours{}".format(len(contours)))
+print(contours)
+imgcopy=img.copy()
+cv.drawContours(imgcopy, contours, -1, (0, 255, 0), 2)
+cv.imshow("Grayscale Image",gray)
+cv.imshow("Drawn Contours",imgcopy)
+cv.imshow("binary Image",binary)
+cv.waitKey(0)
+cv.destroyAllWindows()
